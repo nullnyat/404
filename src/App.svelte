6a4056983430116ded112894a33c78dfff11130d
nullnyat <nullnyat@outlook.com>
2022-09-07 09:47:21 +0900

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import './assets/style.sass'
+    import qrCode from './assets/QR.png'
+
+    let count = 0
+    setInterval(countUp, 30);
+
+    function countUp() {
+        count = Math.min(100, count + 1)
+    }
+</script>
+
+<template lang="pug">
+    h1 :(
+    p.RedirectMessage このページは存在しないよ。5秒後にホームページに移動するよ。
+    p.count {count}%完了
+    .entire
+        img(src='{qrCode}' alt='QR')
+        div
+            .list
+                a(href='https://nullc.at') https://nullc.at
+                p 停止コード: 404
+                p 失敗した内容: PAGE_NOT_FOUND
+</template>
